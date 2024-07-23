613dcb271b84611da4b98767387429e0f630a028
Yandelf00 <125671903+Yandelf00@users.noreply.github.com>
2024-07-23 18:58:20 +0100

    start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="w-full">
-      
+    <main className="w-full justify-center flex flex-row ">
+        <div className="flex flex-col justify-center gap-7 items-center">
+          <div className="w-[2px] h-[180px] bg-gray-100">
+          </div>
+          <h1 className=" rotate-90 text-gray-200 text-[22px] font-light ">HOME</h1>
+        </div>
+        <div className="w-[1250px]">
+
+        </div>
     </main>
   );
 }
